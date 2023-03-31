d446b093a5419185177abb6266abbf53aba653e3
shihasbacker <shihasbacker@yahoo.com>
2023-04-01 00:26:57 +0530

    Navbar added including mobile

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -10,6 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
     </>
   )
 }
