a10b3a50b053e5e72c64035f84de282ae0fb9583
shihasbacker <shihasbacker@yahoo.com>
2023-04-01 10:39:34 +0530

    skills section added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import About from '@/components/About'
 import Main from '@/components/Main'
 import Navbar from '@/components/Navbar'
+import Skills from '@/components/Skills'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -16,6 +17,7 @@ export default function Home() {
       <Navbar />
       <Main />
       <About />
+      <Skills />
     </>
   )
 }
